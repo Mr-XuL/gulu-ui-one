9ce18c1ec4fae3051ddb48c3a3a97bc75ce8777b
Mr-XuL <3200323709@qq.com>
2020-07-31 07:02:12 +0800

    初始化对象

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> hi </div>
+  <Jaxi/>
+  
+</template>
+
+
+<script>
+import  Jaxi from './components/Jaxi.vue'
+
+export default {
+  name: 'App',
+  components: {
+  Jaxi 
+  }
+}
+</script>
