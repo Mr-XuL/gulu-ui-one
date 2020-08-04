7c7827fbadb6348eb833e8c42945967d62ad54d5
Mr-XuL <3200323709@qq.com>
2020-08-04 22:54:22 +0800

    搭建HTML结构

---
+++ src/views/Doc.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div clsss="topnav"> 
+            <div clss="logo"></div>
+            <div clss="menu"></div>
+        </div>
+        
+        <div class="content">
+            <aside>边框</aside>
+            <main>主内容</main>
+        </div>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div clsss="topnav"> 
+            <div clss="logo"></div>
+            <div clss="menu"></div>
+        </div>
+        <div clsss="banner"> 
+            <h1>咕噜-UI</h1>
+            <h2>一个自己制作的UI框架</h2>
+            <p class='actions'>
+                <a href="#">GitHub</a>
+                
+                <a href="#">开始</a>   
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .topnar{ border:1px solid red;}
+</style>
