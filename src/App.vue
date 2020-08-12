fe8b6ae0566b84d87ba405766e5073f896eb0c0e
Mr-XuL <3200323709@qq.com>
2020-08-12 20:32:37 +0800

    更换没有错误的代码

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
-
 <script lang="ts">
-import { ref, provide } from 'vue'
-import { router } from './route'
+import { ref, provide } from "vue";
+import { router } from "./router";
 
 export default {
-  name: 'App',
-  setup(){ 
-    const width = document.documentElement.clientWidth
-    const menuVisible = ref(width <= 500 ? false :true)
-    provide('xxx',menuVisible)
-    if(width <= 500 ){ 
-    router.afterEach(()=>{
-      menuVisible.value=false;
-    })
-    }
-  }
-}
+  name: "App",
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
+    provide("menuVisible", menuVisible); // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
+  },
+};
 </script>
